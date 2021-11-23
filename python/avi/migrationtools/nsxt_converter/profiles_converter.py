932cf259bdcb7900168615529ae91ac7bdad0f6f
Megha Agarwal <agarwalme@vmware.com>
2021-11-23 15:53:14 +0530

    Update profiles_converter.py

--- python/avi/migrationtools/nsxt_converter/profiles_converter.py
+++ python/avi/migrationtools/nsxt_converter/profiles_converter.py
@@ -2,8 +2,8 @@ from avi.migrationtools.nsxt_converter import nsxt_client as nsx_client_util, co
 
 
 def update_alb_type(lb_pr, alb_pr):
-    alb_pr['url'],alb_pr['uuid']=conversion_util.get_obj_url_uuid(lb_pr['path'],lb_pr['unique_id'])
-    tenant=conversion_util.get_tenant_ref(alb_pr['url'])
+    alb_pr['url'],alb_pr['uuid']=conversion_util.get_obj_url_uuid(lb_pr.get('path'),lb_pr.get('unique_id'))
+    tenant=conversion_util.get_tenant_ref(alb_pr.get('url'))
     alb_pr['tenant_ref']=conversion_util.get_object_ref('admin','tenant')
 
     if lb_pr['resource_type'] == 'LBHttpProfile':
@@ -13,21 +13,21 @@ def update_alb_type(lb_pr, alb_pr):
             xff_enabled=lb_pr.get('xForwardedFor'),
             http_to_https=lb_pr.get('httpRedirectToHttps'),
             keepalive_timeout=lb_pr.get('idle_timeout'),
-            client_max_header_size=lb_pr['request_header_size'],
+            client_max_header_size=lb_pr.get('request_header_size'),
             client_max_body_size=lb_pr.get('requestBodySize')
         )
     elif lb_pr['resource_type'] == 'LBFastTcpProfile':
         alb_pr['profile']=dict(
             type = 'APPLICATION_PROFILE_TYPE_TCP',
             tcp_fast_path_profile=dict(
-                session_idle_timeout=lb_pr['idle_timeout'])
+                session_idle_timeout=lb_pr.get('idle_timeout'))
         )
 
     elif lb_pr['resource_type'] == 'LBFastUdpProfile':
         alb_pr['profile'] = dict(
             type='APPLICATION_PROFILE_TYPE_UDP',
             udp_fast_path_profile=dict(
-                session_idle_timeout=lb_pr['idle_timeout']
+                session_idle_timeout=lb_pr.get('idle_timeout')
 
             )
         )
@@ -37,7 +37,7 @@ def convert(alb_config, nsx_lb_config,cloud_name):
     alb_config['NetworkProfile'] = list()
     for lb_pr in nsx_lb_config['LbAppProfiles']:
         alb_pr=dict(
-            name=lb_pr['display_name']
+            name=lb_pr.get('display_name')
         )
         update_alb_type(lb_pr, alb_pr)
         if lb_pr['resource_type'] == 'LBHttpProfile':
